8722cb0bbbad01d69dc0f4796f149e81fd1c442d
SomaTakata <somatakata@gmail.com>
2024-07-08 17:32:03 +0900

    feat: ロード状態を表示するためのLoadingPlaceholderコンポーネントの追加

---
+++ src/components/molecules/LoadingPlaceholder.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const LoadingPlaceholder: React.FC = () => {
+  return (
+    <div className="mt-2 animate-pulse p-4">
+      <div className="mb-2 h-6 w-20 rounded bg-gray-200"></div>
+      <div className="mb-6 h-6 rounded bg-gray-200"></div>
+      <div className="mb-2 h-6 w-20 rounded bg-gray-200"></div>
+      <div className="mb-6 h-6 rounded bg-gray-200"></div>
+      <div className="mb-2 h-6 w-20 rounded bg-gray-200"></div>
+      <div className="mb-6 h-6 rounded bg-gray-200"></div>
+      <div className="mb-2 h-6 w-20 rounded bg-gray-200"></div>
+      <div className="mb-6 h-6 rounded bg-gray-200"></div>
+      <div className="mb-2 h-6 w-20 rounded bg-gray-200"></div>
+      <div className="mb-6 h-6 rounded bg-gray-200"></div>
+      <div className="mb-2 h-6 w-20 rounded bg-gray-200"></div>
+      <div className="mb-6 h-6 rounded bg-gray-200"></div>
+      <div className="mb-2 h-6 w-20 rounded bg-gray-200"></div>
+      <div className="mb-6 h-6 rounded bg-gray-200"></div>
+      <div className="mb-2 h-6 w-20 rounded bg-gray-200"></div>
+      <div className="mb-6 h-6 rounded bg-gray-200"></div>
+    </div>
+  );
+};
+
+export default LoadingPlaceholder;
